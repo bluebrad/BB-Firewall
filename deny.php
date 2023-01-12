f63b1d60299d37a8bf24bf24a5da63a409b8de58
Brad Smith <brad@bluebrad.com>
2023-01-12 10:37:09 -0800

    Create deny.php
    this will allow you to enter the IP and it will generate a new deny ip and if it finds the host blocks them also

---
+++ deny.php
@@ -0,0 +1,41 @@
+<form method="post">
+  <label for="ip_address">Enter IP address to block:</label><br>
+  <input type="text" id="ip_address" name="ip_address"><br>
+  <input type="submit" value="Block IP and hostname">
+</form>
+
+<?php
+if (isset($_POST['ip_address']) && !empty($_POST['ip_address'])) {
+  $ip_address = $_POST['ip_address'];
+
+  // Get hostname associated with IP address
+  $hostname = gethostbyaddr($ip_address);
+
+  // Open .htaccess file in read mode
+  $htaccess_file = fopen('.htaccess', 'r');
+
+  // Read .htaccess file into a string
+  $htaccess_contents = fread($htaccess_file, filesize('.htaccess'));
+
+  // Close .htaccess file
+  fclose($htaccess_file);
+
+  // Insert "Require not ip $ip_address" and "Require not host $hostname" directives before "Require all granted"
+if (!empty($hostname)) {
+  $htaccess_contents = str_replace('Require all granted', "Require not ip $ip_address\nRequire not host $hostname\nRequire all granted", $htaccess_contents);
+} else {
+  $htaccess_contents = str_replace('Require all granted', "Require not ip $ip_address\nRequire all granted", $htaccess_contents);
+}
+
+  // Open .htaccess file in write mode
+  $htaccess_file = fopen('.htaccess', 'w');
+
+  // Write modified .htaccess contents back to file
+  fwrite($htaccess_file, $htaccess_contents);
+
+  // Close .htaccess file
+  fclose($htaccess_file);
+
+  echo "IP address $ip_address and hostname $hostname have been blocked.";
+}
+?>
