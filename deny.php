2f2c70ddb3a71bcabc60195dbbf22a53692595c3
Brad Smith <brad@bluebrad.com>
2023-01-12 10:49:24 -0800

    Update deny.php
    added statcounter to track visits

--- deny.php
+++ deny.php
@@ -38,4 +38,21 @@ if (!empty($hostname)) {
 
   echo "IP address $ip_address and hostname $hostname have been blocked.";
 }
+<!-- Default Statcounter code for bluebrad.net
+http://bluebrad.net -->
+<script type="text/javascript">
+var sc_project=10142707; 
+var sc_invisible=1; 
+var sc_security="e1f9cefd"; 
+</script>
+<script type="text/javascript"
+src="https://www.statcounter.com/counter/counter.js"
+async></script>
+<noscript><div class="statcounter"><a title="Web Analytics
+Made Easy - Statcounter" href="https://statcounter.com/"
+target="_blank"><img class="statcounter"
+src="https://c.statcounter.com/10142707/0/e1f9cefd/1/"
+alt="Web Analytics Made Easy - Statcounter"
+referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
+<!-- End of Statcounter Code -->
 ?>
